f41dde7ee876fb4163de04a20ba5a3996db6b0d8
김준성 <anima94@naver.com>
2020-12-13 15:41:22 +0900

    Executors 까지

--- README.md
+++ README.md
@@ -366,3 +366,36 @@ System.out.println("hello : " + Thread.currentThread().getName());
 - 다른 쓰레드 깨우기(interupt): 다른쓰레드를 깨워서 interruptedException을 발생시킴.
 그 에러가 발생했을때 할 일은 코딩하기 나름
 - 다른 쓰레드 기다리기(join): 다른쓰레드가 끝날때까지 기다린다.
+
+# 15. Executors
+### 고수준(High-Level) Concurrency프로그래밍
+- 쓰레드를 만들고 관리하는 작업을 애플리케이션에서 분리
+- 그런 기능을 Executors에게 위임
+
+### Executors가 하는 일
+- 쓰레드 만들기: 애플리케이션이 사용할 쓰레드 풀을 만들어 관리한다.
+- 쓰레드 관리: 쓰레드 생명주기를 관리한다.
+- 작업처리 및 실행: 쓰레드로 실행할 작업을 제공할 수 있는 API를 제공한다.
+
+### 주요 인터페이스
+- Executor: execute(Runnable)
+- ExecutorService: Executor 상속받은 인터페이스로 , Callable도 실행할 수 있으며, Executor를 종료시키거나
+여러 Callable을 동시에 실행하는 드의 기능을 제공한다. 
+- ScheduledExecutorService: ExecutorService를 상속받은 인터페이스로 특정시간 이후에 주기적으로 작업을 실행할 수 있다.
+
+### ExecutorService로 작업 실행하기
+```
+ExecutorService executorService = Executors.newSingleThreadExecutor();
+executorService.submit(() -> {
+System.out.println("Hello :" + Thread.currentThread().getName());
+});
+```
+
+### ExecutorService로 멈추기
+```
+executorService.shutdown(); // 처리중인 작업 기다렸다가 종료
+executorService.shutdownNow(); // 당장 종료
+```
+
+### Fork/Join 프레임워크
+- ExecutorService의 구현체로 손쉽게 멀티 프로세서를 활용할 수 있게끔 도와준다.
--- src/main/java/com/example/demo/conCurrent/ConcurrentApp.java
+++ src/main/java/com/example/demo/conCurrent/ConcurrentApp.java
@@ -1,5 +1,10 @@
 package com.example.demo.conCurrent;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ConcurrentApp {
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName()); //메인쓰레드
@@ -25,6 +30,32 @@ public class ConcurrentApp {
         //thread.interrupt();
         thread.join(); //기다렸다가 그다음라인이 실행됨.
         System.out.println(thread+ " is finished");
+
+        //=================Executors===================
+        System.out.println("=================Executors===================");
+        ExecutorService executorService= Executors.newSingleThreadExecutor();
+        executorService.submit(() ->
+                System.out.println("Thread "+Thread.currentThread().getName()));
+        //executorService.shutdown();
+
+
+        ExecutorService executorService1=Executors.newFixedThreadPool(2);
+        executorService1.submit(getRunnable("Hello"));
+        executorService1.submit(getRunnable("Junseong"));
+        executorService1.submit(getRunnable("The"));
+        executorService1.submit(getRunnable("Java"));
+        executorService1.submit(getRunnable("Thread"));
+
+
+        ScheduledExecutorService executorService2=Executors.newSingleThreadScheduledExecutor();
+        executorService2.scheduleAtFixedRate(getRunnable("Hello"),1,2, TimeUnit.DAYS);
+
+
+       /// executorService1.shutdown();
+
+    }
+    private static Runnable getRunnable(String message){
+        return () -> System.out.println(message+ Thread.currentThread().getName());
     }
 
     static class MyThread extends Thread{ //1번째 방법
